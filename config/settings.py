a611654e083bdad93b2f71724754126ceed78e2f
Ali Bahadorani <a.bahadorani@outlook.com>
2025-02-25 20:01:06 +0330

    feat: Add ClientManager class to handle different client from different model provider/source

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,9 @@ load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
